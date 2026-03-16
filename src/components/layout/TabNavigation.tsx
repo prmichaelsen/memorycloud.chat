161e9f4f1a0e0f2eaa3c917c1040dfaebd4f859a
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2026-03-16 05:45:45 +0000

    style(sidebar): increase vertical spacing between nav items

--- src/components/layout/TabNavigation.tsx
+++ src/components/layout/TabNavigation.tsx
@@ -36,7 +36,7 @@ export function TabNavigation({
 
   const isVertical = orientation === 'vertical'
   const containerClass = isVertical
-    ? 'flex flex-col gap-1'
+    ? 'flex flex-col gap-3'
     : 'flex items-center gap-1'
 
   return (
